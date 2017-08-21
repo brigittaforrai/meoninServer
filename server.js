6f91121bd5dbff346dcfd6ff3703102a147b67a4
brigittaforrai <forraibrigi@gmail.com>
2017-08-22 00:54:19 +0200

    -

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-var mailSender = require('./index.js');
+var mailSender = require('./mail.js');
 
 app.get('https://hidden-earth-71841.herokuapp.com/', function (req, res) {
   // var email = req.body.email;
@@ -15,5 +15,5 @@ app.get('https://hidden-earth-71841.herokuapp.com/', function (req, res) {
 
 let port = process.env.PORT || 5000;
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 5000!')
 })

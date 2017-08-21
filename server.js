b6d88e067b5cbd88cde16a5235a1f60def839c00
brigittaforrai <forraibrigi@gmail.com>
2017-08-21 20:07:52 +0200

    express js

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+
+var mailSender = require('./index.js');
+
+app.get('/', function (req, res) {
+  // var email = req.body.email;
+  res.send('GET request to the homepage')
+})
+
+// mailSender();
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
